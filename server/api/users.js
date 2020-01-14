1d561904ec1d35e60398a2598cc056f429471a3a
Kevin Thai <kevin.k.thai@gmail.com>
2020-01-14 09:34:33 -0600

    Initial commit

--- server/api/users.js
+++ server/api/users.js
@@ -3,11 +3,9 @@ const {User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
+  //Authenticate that the user is an admin in order to access all users
   try {
     const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
       attributes: ['id', 'email']
     })
     res.json(users)
