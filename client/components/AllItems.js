d263ff750abbee273124d862315237e1ef00832a
vjt4 <54646079+vjt4@users.noreply.github.com>
2020-01-15 16:20:32 -0600

    All item vertical slice done (#8)
    * inertial kermit
    
    * address doneish
    
    * added models
    
    * unpluralized model names
    
    * setting up relations
    
    * setting up relations
    
    * now force sync
    
    * relations are set up
    
    * removed force true from server index.js
    
    * start of seed files and model changes
    
    * removed cart model and its relations to other models
    
    * added "carted to the validation in order"
    
    * removed the require cart from seed.js
    
    * worked on seed file
    
    * removed order user relation
    
    * just getting started on the store for item
    
    * AllItems & ItemCard components
    
    * added api route, store, thunks etc for items
    
    * work on SingleItem view
    
    * All Items store redux good
    
    * allItems, view backend etc operational
    
    * addressed comments v1.1
    
    Co-authored-by: pozag <49734340+pozag@users.noreply.github.com>
    Co-authored-by: Kristen <kristen.andersen@gmail.com>
    Co-authored-by: Kevin Thai <kevin.k.thai@gmail.com>

---
+++ client/components/AllItems.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import ItemCard from './ItemCard'
+import {fetchItems} from './../store/item'
+import {connect} from 'react-redux'
+
+class AllItems extends Component {
+  componentDidMount() {
+    this.props.loadItems()
+  }
+
+  //needs onSubmit: add to cart to send as props
+  render() {
+    return (
+      <div className="centered-parent">
+        <div className="custom-card-list ui cards">
+          {this.props.items.length ? (
+            this.props.items.map(item => {
+              return (
+                <div key={item.id}>
+                  <ItemCard {...item} />
+                </div>
+              )
+            })
+          ) : (
+            <div> No Items</div>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    items: state.items
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    loadItems: () => dispatch(fetchItems())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatch)(AllItems)
