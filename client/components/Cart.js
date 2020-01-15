4f1f1164a9a63779537d4338afbebfd5a00def14
reuben <reubenthegold@gmail.com>
2020-01-15 11:57:08 -0600

    initial commit

---
+++ client/components/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+const Cart = () => <div>Hello World</div>
+
+const mapState = state => ({})
+
+const mapDispatch = dispatch => ({})
+
+export default connect(mapState, mapDispatch)(Cart)
