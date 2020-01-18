a7c97cd8eb5a28422b35f4271b7d933abe4748c6
Virge Trotter <vtrotter@u.rochester.edu>
2020-01-18 12:27:57 -0600

    admin order view displays unregistered if there is no user for the order

--- client/components/OrderCard.js
+++ client/components/OrderCard.js
@@ -7,7 +7,7 @@ const OrderCard = ({order}) => {
       <Link to={`/orders/${order.id}`} className="order">
         <h1>{order.id}</h1>
         <h1>{order.status}</h1>
-        <h1>{order.user.email}</h1>
+        {order.user ? <h1>{order.user.email}</h1> : <h1> 'unregistered'</h1>}
       </Link>
     </div>
   )
