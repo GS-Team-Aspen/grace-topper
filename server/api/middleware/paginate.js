76d5aa32ab655bc4a63d8b6dacb1c5069a1983bf
Kevin Thai <kevin.k.thai@gmail.com>
2020-01-20 00:06:46 -0600

    Modified the pagiante function

--- server/api/middleware/paginate.js
+++ server/api/middleware/paginate.js
@@ -4,18 +4,8 @@ module.exports = (content, page, limit) => {
 
   const results = {}
 
-  if (end < content.length) {
-    results.next = {
-      page: page + 1,
-      limit
-    }
-  }
-  if (start > 0) {
-    results.previous = {
-      page: page - 1,
-      limit
-    }
-  }
+  if (end < content.length) results.next = page + 1
+  if (start > 0) results.previous = page - 1
 
   results.data = content.slice(start, end)
   return results
